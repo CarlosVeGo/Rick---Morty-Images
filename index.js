0d9efc3702b2755ca2c1d3e47f93f46992909609
CarlosVeGo <carlos.velezg2011@gmail.com>
2024-04-24 16:51:04 -0500

    botones next y back añadidos

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 const container = document.querySelector(".container");
-const apiLink = `https://rickandmortyapi.com/api/character`;
+const next = document.querySelector(".next");
+const back = document.querySelector(".back")
+const botones = document.querySelector(".botones");
+let page = 1;
+let apiLink = `https://rickandmortyapi.com/api/character/?page=${page}`;
 
-
-
-fetch(apiLink).then((data)=>data.json().then((res)=>{
-    console.log(res)
-    const results = res.results;
-    
-
-    results.forEach(result => { 
+function showPage(link){
+    fetch(link).then((data)=>data.json().then((res)=>{
         
-        const linkImage = result.image;
-        const name = result.name;
-        container.innerHTML +=`
-        <div class="card">
-            <span>${name}</span>
-            <img src="${linkImage}" alt="imagen de ${name}">
-        `
-        console.log(result)
-    });
+        const results = res.results;
+        container.innerHTML =``;
+        
+        results.forEach(result => { 
+            
+            const linkImage = result.image;
+            const name = result.name;
+            container.innerHTML +=`
+            <div class="card">
+                <span>${name}</span>
+                <img src="${linkImage}" alt="imagen de ${name}">
+            `
+        });
+    
+    })).catch((err)=>container.innerHTML=`try again later`)
+    
+}
 
-}))
+showPage(apiLink)
 
+next?.addEventListener("click",() => {
+
+    container.innerHTML = `Loading Images...`;
+    page++;
+    apiLink = `https://rickandmortyapi.com/api/character/?page=${page}`;
+
+    showPage(apiLink);
+
+    back.classList.remove("dis");
+
+    page === 42 ? next.classList.add("dis"):null;
+
+})
+
+back?.addEventListener("click",() => {
+    
+    container.innerHTML = `Loading Images...`;
+    page === 42 ? next.classList.remove("dis"):null;
+    
+    page--;
+    
+    apiLink = `https://rickandmortyapi.com/api/character/?page=${page}`;
+
+    showPage(apiLink);
+
+    page === 1 ? back.classList.add("dis"):null;
+
+})
